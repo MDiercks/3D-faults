30b82597ffcaff3848eac969589c1da395f8473c
Manuel Diercks <54583914+MDiercks@users.noreply.github.com>
2025-08-29 15:11:59 +0200

    version 2.9.0

--- faults_3D.m
+++ faults_3D.m
@@ -1,15 +1,28 @@
-%% Script to calculate and plot faults and project faults to depth, gridded according to the specified grid size
-% Written by Zoe Mildon, 2016
-% Code is free to use for research purposes, please cite the following paper:
+%% 3D-Faults v. 2.9
+%
+% Software to plot 3D-faults from surface traces, assign slip distributions and export for use in Coulomb 3.3 software
+% 
+% This code is free to use for research purposes, please cite the following papers:
+%
+% Diercks, M., Mildon, Z. K., Boulton, S. J., Hussain, E., Alçiçek, M. C., Yıldırım, C., & Aykut, T. (2023). 
+% Constraining historical earthquake sequences with Coulomb stress models: An example from western Türkiye. 
+% Journal of Geophysical Research: Solid Earth, 128(11), e2023JB026627.
+%
+% and the original article:
+%
 % Mildon, Z. K., S. Toda, J. P. Faure Walker, and G. P. Roberts (2016) 
 % Evaluating models of Coulomb stress transfer- is variable fault geometry important?
 % Geophys. Res. Lett., 43, doi:10.1002/2016GL071128.
 % 
-% version 2 written in 2021-2023 by Manuel Diercks and Zoe Mildon
+% version 1 written by Zoe Mildon, 2016
+% version 2 written in 2021-2024 by Manuel Diercks and Zoe Mildon
 %
-% To execute the code press F5 or enter 'faults_3D' in the command line.
+% For updates check github.com/MDiercks/
+% For bug reports or queries please contact diercks@geowi.uni-hannover.de
 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% To execute the code press F5 or enter 'faults_3D' in the command window.
+%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%        DO NOT CHANGE ANY CODE FOR NORMAL OPERATION      %%%%%%%%%%%
 addpath Code/
 addpath Fault_traces/
