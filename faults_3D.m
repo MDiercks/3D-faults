4939fd1a613135bbd2cc78dbbb0bee06a26d998c
Manuel Diercks <54583914+MDiercks@users.noreply.github.com>
2026-06-04 11:55:27 +0200

    v. 2.10.04

--- faults_3D.m
+++ faults_3D.m
@@ -1,4 +1,4 @@
-%% 3D-Faults v. 2.10.2 | 05/2026
+%% 3D-Faults v. 2.10.04 | 06/2026
 %
 % Software to plot 3D-faults from surface traces, assign slip distributions and export for use in Coulomb 3.3 software
 % 
